92b5bedc1e7d4e4448156e6ec088c7bfdba97653
Mark <m12m3r@gmail.com>
2023-06-01 01:27:32 +0100

    part2

---
+++ Makefile
@@ -0,0 +1,2 @@
+db: db.c
+	$(CC) db.c -o db -Wall -Wextra -pedantic -std=c99
--- db.c
+++ db.c
@@ -19,6 +19,15 @@ typedef enum {
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
+
 InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
     input_buffer->buffer = NULL;
@@ -50,6 +59,39 @@ void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    
+    if (strncmp(input_buffer->buffer, "select", 6) == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+    switch (statement->type) {
+        case STATEMENT_INSERT:
+            printf("Insert Statement\n");
+            break;
+        case STATEMENT_SELECT:
+            printf("Select Statement\n");
+            break;
+    }
+}
+
 int main(int argc, char* argv[]) {
     InputBuffer* input_buffer = new_input_buffer();
     while(true) {
@@ -58,9 +100,25 @@ int main(int argc, char* argv[]) {
 
         if (input_buffer->buffer[0] == '.') {
             switch (do_meta_command(input_buffer)) {
-                case ()
+                case META_COMMAND_SUCCESS:
+                    continue;
+                case META_COMMAND_UNRECOGNIZED_COMMAND:
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;
             }
         }
+
+        Statement* statement = (Statement*)malloc(sizeof(Statement));
+        switch (prepare_statement(input_buffer, statement)) {
+            case PREPARE_SUCCESS:
+                break;
+            case PREPARE_UNRECOGNIZED_STATEMENT:
+                printf("Unrecognized keyword at start of '%s'\n", input_buffer->buffer);
+                continue;
+        }
+
+        execute_statement(statement);
+        printf("Executed.\n");
     }
 }
 
